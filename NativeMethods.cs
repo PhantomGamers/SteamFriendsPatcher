7e3189b1d8a2b372352d6e57c5efc2f84a765a86
Chris Yeninas <phantom@sundergaming.com>
2019-06-14 02:07:59 -0400

    Fix security vulnerability CA2101

--- NativeMethods.cs
+++ NativeMethods.cs
@@ -8,7 +8,7 @@ namespace SteamFriendsPatcher
         [DllImport("msvcrt.dll", EntryPoint = "memcmp", CallingConvention = CallingConvention.Cdecl)]
         public static extern int Memcmp(byte[] b1, byte[] b2, long count);
 
-        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true, CharSet = CharSet.Unicode, ThrowOnUnmappableChar = true)]
         public static extern IntPtr FindWindowByClass(string lpClassName, IntPtr ZeroOnly = default);
 
         public const int HWND_BROADCAST = 0xffff;
@@ -17,7 +17,7 @@ namespace SteamFriendsPatcher
         [DllImport("user32")]
         public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
 
-        [DllImport("user32")]
+        [DllImport("user32", CharSet = CharSet.Unicode, ThrowOnUnmappableChar = true)]
         public static extern int RegisterWindowMessage(string message);
     }
 }
